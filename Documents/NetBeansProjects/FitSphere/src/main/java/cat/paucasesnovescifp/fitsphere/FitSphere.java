732c023e00d6c07c440e0006707f1e31a13b0d96
Alejandro <alejandro.mendozasegui@gmail.com>
2024-10-11 13:55:02 +0200

    Commit Inicial

---
+++ Documents/NetBeansProjects/FitSphere/src/main/java/cat/paucasesnovescifp/fitsphere/FitSphere.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cat.paucasesnovescifp.fitsphere;
+
+/**
+ *
+ * @author aleja
+ */
+public class FitSphere {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+       
+    }
+}
